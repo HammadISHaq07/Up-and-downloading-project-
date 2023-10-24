e3729bc71729b7836baf3438385db539584c78f2
HammadISHaq07 <148836623+HammadISHaq07@users.noreply.github.com>
2023-10-24 08:40:27 +0500

    Api for upload and download web dot net

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using FileUploadDownloadAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace FileUploadDownloadAPI.ApplicatioDbContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+      
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<FileModel> Files { get; set; }
+    }
+}
---
+++ FilesController.cs
@@ -0,0 +1,56 @@
+﻿using FileUploadDownloadAPI.ApplicatioDbContext;
+using FileUploadDownloadAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileUploadDownloadAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FilesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [Route("upload")]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest("Invalid file");
+
+            using (var memoryStream = new MemoryStream())
+            {
+                await file.CopyToAsync(memoryStream);
+
+                var fileModel = new FileModel
+                {
+                    FileName = file.FileName,
+                    FileContent = memoryStream.ToArray()
+                };
+
+                _context.Files.Add(fileModel);
+                await _context.SaveChangesAsync();
+
+                return Ok(new { fileModel.Id });
+            }
+        }
+
+        [HttpGet]
+       [Route("download/{id}")]
+        public IActionResult Download(int id)
+        {
+            var file = _context.Files.FirstOrDefault(f => f.Id == id);
+
+            if (file == null)
+                return NotFound();
+
+            return File(file.FileContent, "application/octet-stream", file.FileName);
+        }
+    }
+}
+    
