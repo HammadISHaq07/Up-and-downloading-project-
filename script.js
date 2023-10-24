b39d163b322f1e2846e90dd4e58d3b1fe9194391
HammadISHaq07 <148836623+HammadISHaq07@users.noreply.github.com>
2023-10-24 08:46:05 +0500

    front of Ai

--- script.js
+++ script.js
@@ -1,85 +1,85 @@
-const chatbotToggler = document.querySelector(".chatbot-toggler");
-const closeBtn = document.querySelector(".close-btn");
-const chatbox = document.querySelector(".chatbox");
-const chatInput = document.querySelector(".chat-input textarea");
-const sendChatBtn = document.querySelector(".chat-input span");
+const generateForm = document.querySelector(".generate-form");
+const generateBtn = generateForm.querySelector(".generate-btn");
+const imageGallery = document.querySelector(".image-gallery");
 
-let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
-const inputInitHeight = chatInput.scrollHeight;
+const OPENAI_API_KEY = "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"; // Your OpenAI API key here
+let isImageGenerating = false;
 
-const createChatLi = (message, className) => {
-    // Create a chat <li> element with passed message and className
-    const chatLi = document.createElement("li");
-    chatLi.classList.add("chat", `${className}`);
-    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
-    chatLi.innerHTML = chatContent;
-    chatLi.querySelector("p").textContent = message;
-    return chatLi; // return chat <li> element
-}
-
-const generateResponse = (chatElement) => {
-    const API_URL = "https://api.openai.com/v1/chat/completions";
-    const messageElement = chatElement.querySelector("p");
-
-    // Define the properties and message for the API request
-    const requestOptions = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${API_KEY}`
-        },
-        body: JSON.stringify({
-            model: "gpt-3.5-turbo",
-            messages: [{role: "user", content: userMessage}],
-        })
-    }
-
-    // Send POST request to API, get response and set the reponse as paragraph text
-    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
-        messageElement.textContent = data.choices[0].message.content.trim();
-    }).catch(() => {
-        messageElement.classList.add("error");
-        messageElement.textContent = "Oops! Something went wrong. Please try again.";
-    }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
-}
-
-const handleChat = () => {
-    userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
-    if(!userMessage) return;
-
-    // Clear the input textarea and set its height to default
-    chatInput.value = "";
-    chatInput.style.height = `${inputInitHeight}px`;
-
-    // Append the user's message to the chatbox
-    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
-    chatbox.scrollTo(0, chatbox.scrollHeight);
+const updateImageCard = (imgDataArray) => {
+  imgDataArray.forEach((imgObject, index) => {
+    const imgCard = imageGallery.querySelectorAll(".img-card")[index];
+    const imgElement = imgCard.querySelector("img");
+    const downloadBtn = imgCard.querySelector(".download-btn");
     
-    setTimeout(() => {
-        // Display "Thinking..." message while waiting for the response
-        const incomingChatLi = createChatLi("Thinking...", "incoming");
-        chatbox.appendChild(incomingChatLi);
-        chatbox.scrollTo(0, chatbox.scrollHeight);
-        generateResponse(incomingChatLi);
-    }, 600);
+    // Set the image source to the AI-generated image data
+    const aiGeneratedImage = `data:image/jpeg;base64,${imgObject.b64_json}`;
+    imgElement.src = aiGeneratedImage;
+    
+    // When the image is loaded, remove the loading class and set download attributes
+    imgElement.onload = () => {
+      imgCard.classList.remove("loading");
+      downloadBtn.setAttribute("href", aiGeneratedImage);
+      downloadBtn.setAttribute("download", `${new Date().getTime()}.jpg`);
+    }
+  });
 }
 
-chatInput.addEventListener("input", () => {
-    // Adjust the height of the input textarea based on its content
-    chatInput.style.height = `${inputInitHeight}px`;
-    chatInput.style.height = `${chatInput.scrollHeight}px`;
-});
+const generateAiImages = async (userPrompt, userImgQuantity) => {
+  try {
+    // Send a request to the OpenAI API to generate images based on user inputs
+    const response = await fetch("https://api.openai.com/v1/images/generations", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        prompt: userPrompt,
+        n: userImgQuantity,
+        size: "512x512",
+        response_format: "b64_json"
+      }),
+    });
 
-chatInput.addEventListener("keydown", (e) => {
-    // If Enter key is pressed without Shift key and the window 
-    // width is greater than 800px, handle the chat
-    if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
-        e.preventDefault();
-        handleChat();
-    }
-});
+    // Throw an error message if the API response is unsuccessful
+    if(!response.ok) throw new Error("Failed to generate AI images. Make sure your API key is valid.");
 
-sendChatBtn.addEventListener("click", handleChat);
-closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
-chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
+    const { data } = await response.json(); // Get data from the response
+    updateImageCard([...data]);
+  } catch (error) {
+    alert(error.message);
+  } finally {
+    generateBtn.removeAttribute("disabled");
+    generateBtn.innerText = "Generate";
+    isImageGenerating = false;
+  }
+}
+
+const handleImageGeneration = (e) => {
+  e.preventDefault();
+  if(isImageGenerating) return;
+
+  // Get user input and image quantity values
+  const userPrompt = e.srcElement[0].value;
+  const userImgQuantity = parseInt(e.srcElement[1].value);
+  
+  // Disable the generate button, update its text, and set the flag
+  generateBtn.setAttribute("disabled", true);
+  generateBtn.innerText = "Generating";
+  isImageGenerating = true;
+  
+  // Creating HTML markup for image cards with loading state
+  const imgCardMarkup = Array.from({ length: userImgQuantity }, () => 
+      `<div class="img-card loading">
+        <img src="images/loader.svg" alt="AI generated image">
+        <a class="download-btn" href="#">
+          <img src="images/download.svg" alt="download icon">
+        </a>
+      </div>`
+  ).join("");
+
+  imageGallery.innerHTML = imgCardMarkup;
+  generateAiImages(userPrompt, userImgQuantity);
+}
+
+generateForm.addEventListener("submit", handleImageGeneration);
